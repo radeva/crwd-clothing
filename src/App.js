41e32cfe9edbbac83c61d9fcd817a1063b87f9fe
Veselina Radeva <veselina.radeva@telerik.com>
2020-05-22 17:33:42 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, useParams} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const AnyPage = () => {
+  let { pageName } = useParams();
+
+  return (<div><h1>{pageName} Page</h1></div>)
+}
 
 function App() {
   return (
     <div>
-     <HomePage />
+      <Router>
+        <Switch>
+          <Route path='/shop/:pageName' component={AnyPage} />
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
